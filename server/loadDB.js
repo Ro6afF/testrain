ddd56b66f54baa8de3910cd5def95fab40cf674e
Ro6afF <dimo_chanev@windowslive.com>
2017-10-20 21:18:47 +0300

    Added DB

---
+++ server/loadDB.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const mongodb = require('mongodb');
+
+let MongoClient = mongodb.MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/testrain', (err, db) => {
+    console.log('Connecting...');
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log('Connected ^_^');
+    console.log('Loading data from \'db.json\'');
+    const data = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
+    console.log('Loaded ^_^');
+    for (const i of data) {
+        console.log('Loading ' + i.name);
+        db.collection(i.name).insertMany(i.data);
+        console.log(i.name + ' loaded successfuly ^_^');
+    }
+    db.close();
+    console.log('DONE ^_^');
+});
